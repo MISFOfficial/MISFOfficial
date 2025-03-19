51ef9ba2dc8499e4d450c6f0242aa2461e9f06e1
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-03-19 13:12:53 +0600

    modify

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,12 @@
-const hideMenu=()=>{
-    console.log('Jahin')
-}
+function toggleMenu() {
+    const menuIcon = document.getElementById('menuIcon');
+    const closeIcon = document.getElementById('closeIcon');
+    const hideNavigation = document.getElementById('hideNavigation');
+  
+    // Toggle visibility of menu and close icons
+    menuIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+  
+    // Toggle visibility of the navigation menu
+    hideNavigation.classList.toggle('hidden');
+  }
