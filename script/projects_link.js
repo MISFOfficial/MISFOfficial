71d0e3ff311bf2c8d380525743592fa574d8f369
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-04-01 16:19:36 +0600

    modify

---
+++ script/projects_link.js
@@ -0,0 +1,8 @@
+// Project 1
+const seo=()=>{
+    window.open('https://misfofficial.github.io/Helth-care-SEO-Service/', '_blank')
+}
+// Project 2
+const engJanala=()=>{
+    window.open('https://misfofficial.github.io/B11-A6-Eng-Janala/', '_blank')
+}
